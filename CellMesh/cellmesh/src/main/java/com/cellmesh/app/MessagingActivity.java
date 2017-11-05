4fb50ad5760aafa9fe25fda6506d25d4aa956f91
Benjamin Holland <ben.holland.core@gmail.com>
2017-11-05 03:37:36 -0800

    Organized imports, removed original activity files

--- CellMesh/cellmesh/src/main/java/com/cellmesh/app/MessagingActivity.java
+++ CellMesh/cellmesh/src/main/java/com/cellmesh/app/MessagingActivity.java
@@ -3,7 +3,6 @@ package com.cellmesh.app;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,12 +12,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.cellmesh.app.model.INodeListener;
+import com.cellmesh.app.model.Node;
+
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
-
-import com.cellmesh.app.model.INodeListener;
-import com.cellmesh.app.model.Node;
 
 public class MessagingActivity extends Activity implements INodeListener
 {
